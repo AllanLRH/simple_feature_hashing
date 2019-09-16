204857e51f151c91b10534a2cd61dc149b4a67ce
Allan L. R. Hansen <allan.lrh@gmail.com>
2019-09-16 21:29:10 +0200

    Spelling end foratting

--- simple_feature_hashing.py
+++ simple_feature_hashing.py
@@ -14,7 +14,7 @@ from time import time
 
 t0 = time()
 # Create data
-n_prones = 100_000 if len(sys.argv) == 1 else int(sys.argv[1])
+n_phones = 100_000 if len(sys.argv) == 1 else int(sys.argv[1])
 phones = """Iphone X
 Iphone 8 Max
 Samsung Galaxy S10 Plus
@@ -26,10 +26,10 @@ LG V40 ThinQ
 LG G7 Plus ThinQ
 Google Pixel 2 XL
 OnePlus 6T""".splitlines()
-ser = pd.Series([phones[i] for i in np.random.randint(0, len(phones), (n_prones,))])
+ser = pd.Series([phones[i] for i in np.random.randint(0, len(phones), (n_phones,))])
 
 # This is an optimization, but the problem seems to be memory bound rather than CPU bound (the hashing must be cheap)
-if n_prones < 10_000:
+if n_phones < 10_000:
     hsh = ser.apply(hash).values
 else:
     unique_phones = ser.unique()
@@ -38,11 +38,14 @@ else:
 
 assert pd.unique(hsh).shape[0] == ser.nunique()
 
-hshv = np.unpackbits(hsh.view(np.uint8))  # open a view into the continious memory, interpret as uint8
+hshv = np.unpackbits(
+    hsh.view(np.uint8)
+)  # open a view into the continious memory, interpret as uint8
 
 # This is our new feature
 feat = hshv.reshape(ser.shape[0], 64)  # 64 because the hast values are really int64
 
+print(feat)
 assert len({tuple(el) for el in feat}) == len(phones)
 t1 = time()
 print(t1 - t0)
